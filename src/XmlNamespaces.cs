9a94af65011819b512b654f35ffc2fea41b6d884
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-01-31 20:17:36 +0100

    Gitty update

--- src/XmlNamespaces.cs
+++ src/XmlNamespaces.cs
@@ -1,4 +1,5 @@
-﻿namespace Aspenlaub.Net.GitHub.CSharp.Protch {
+﻿// ReSharper disable UnusedMember.Global
+namespace Aspenlaub.Net.GitHub.CSharp.Protch {
     public class XmlNamespaces {
         public const string CsProjNamespaceUri = @"http://schemas.microsoft.com/developer/msbuild/2003";
         public const string NuSpecNamespaceUri = @"http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd";
