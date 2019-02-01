569f5e91802e18284b20c96b59eb0a5364cd485d
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-02-01 19:00:35 +0100

    Support Microsoft.Extensions.DependencyInjection

--- src/ProtchContainerBuilder.cs
+++ src/ProtchContainerBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using Aspenlaub.Net.GitHub.CSharp.Protch.Interfaces;
 using Autofac;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Protch {
     public static class ProtchContainerBuilder {
@@ -7,6 +8,12 @@ namespace Aspenlaub.Net.GitHub.CSharp.Protch {
             builder.RegisterType<ProjectFactory>().As<IProjectFactory>();
             builder.RegisterType<ProjectLogic>().As<IProjectLogic>();
             return builder;
+        }
+        // ReSharper disable once UnusedMember.Global
+        public static IServiceCollection UseProtch(this IServiceCollection services) {
+            services.AddTransient<IProjectFactory, ProjectFactory>();
+            services.AddTransient<IProjectLogic, ProjectLogic>();
+            return services;
         }
     }
 }
