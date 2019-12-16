8734459b5f3b01a7e9ba40f4a14eeafe0f200d93
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-12-16 20:29:16 +0100

    Test case failed

--- src/Test/ProjectFactoryTest.cs
+++ src/Test/ProjectFactoryTest.cs
@@ -78,7 +78,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.Protch.Test {
             Assert.AreEqual("git", project.RepositoryType);
             Assert.AreEqual(url, project.RepositoryUrl);
             Assert.AreEqual("master", project.RepositoryBranch);
-            Assert.AreEqual("", project.PackageId);
+            Assert.AreEqual("PakledConsumerCore", project.PackageId);
             var rootNamespace = "";
             foreach (var propertyGroup in project.PropertyGroups) {
                 Assert.IsNotNull(propertyGroup);
